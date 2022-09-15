287f520b2f3cf2a9445d88069a276e683c416f0e
Ryan Clark <ryan@ryanclark.me>
2022-09-15 15:25:16 +0100

    Change `NewWindowsDesktopServiceV3` to accept `Metadata` on creation
    This allows us to add labels to the `windows_desktop_service` section of
    the config and store them inside `WindowsDesktopServiceV3`.

--- lib/service/cfg.go
+++ lib/service/cfg.go
@@ -1169,6 +1169,7 @@ type WindowsDesktopConfig struct {
 	ConnLimiter limiter.Config
 	// HostLabels specifies rules that are used to apply labels to Windows hosts.
 	HostLabels HostLabelRules
+	Labels     map[string]string
 }
 
 type LDAPDiscoveryConfig struct {
