37dda16aa06313f2a468befa9c5801e5f4cca8bf
Ryan Clark <ryan@ryanclark.me>
2022-09-16 18:11:11 +0100

    log error

--- tool/tctl/common/resource_command.go
+++ tool/tctl/common/resource_command.go
@@ -763,6 +763,7 @@ func (rc *ResourceCommand) Delete(ctx context.Context, client auth.ClientI) (err
 		for _, desktop := range desktops {
 			if desktop.GetName() != rc.ref.Name {
 				if err = client.DeleteWindowsDesktop(ctx, desktop.GetHostID(), rc.ref.Name); err != nil {
+					fmt.Println("errr", err)
 					errs = append(errs, err)
 					continue
 				}
