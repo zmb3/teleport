c2876ea3b31520c45ce605ff1a07e2a2bfbe10a9
Zac Bergquist <zac.bergquist@goteleport.com>
2022-09-25 16:33:46 -0600

    Run a single uploader service per process (#14521)
    Prior to this change, each individual service (proxy, app, SSH, db, etc)
    would spin up its own uploader service. If you are running multiple
    Teleport services in the same process, this means you get multiple
    uploaders all looking at the same directory, which can result in
    duplicate upload events in the audit log.
    
    Additionally, desktop access has (mistakenly) failed to set up this
    service, so desktop sessions would only be uploaded if you happened
    to also run some other service in the same process that does spin up
    the uploader.
    
    Solve these issues by centralizing the uploader service so that it
    runs once per process, and each Teleport service doesn't need to think
    about whether or not the service should run.

--- lib/events/filesessions/fileasync_chaos_test.go
+++ lib/events/filesessions/fileasync_chaos_test.go
@@ -121,7 +121,6 @@ func TestChaosUpload(t *testing.T) {
 		ScanPeriod: scanPeriod,
 		Streamer:   faultyStreamer,
 		Clock:      clock,
-		AuditLog:   &events.DiscardAuditLog{},
 	})
 	require.NoError(t, err)
 	go uploader.Serve(ctx)
